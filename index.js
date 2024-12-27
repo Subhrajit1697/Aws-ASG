b418aa008b22dde9e43dd93ac5ed74df0c985cf0
Subhrajit Chandra <2414101844@mujonline.edu.in>
2024-12-27 15:56:49 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/load', (req, res) => {
     // simulate a load on cpu
     const start = Date.now();
     while (Date.now() - start < 80000); // Simulate CPU load for 80 seconds
