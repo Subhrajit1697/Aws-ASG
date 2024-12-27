2c535e8f03af2b396009d122ac6d3a0409fa643f
Subhrajit Chandra <2414101844@mujonline.edu.in>
2024-12-27 15:45:19 +0530

    update

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    // simulate a load on cpu
+    const start = Date.now();
+    while (Date.now() - start < 80000); // Simulate CPU load for 80 seconds
+    res.send('CPU load simulation complete');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
